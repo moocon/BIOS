4d8f12ea0c91584606685b23d547feeb6b7ac9e1
moocon <moocon@yeah.net>
2017-11-17 08:10:24 +0800

    Update pci_1.c

--- PCI/pci_1.c
+++ PCI/pci_1.c
@@ -38,30 +38,31 @@ void PCI_modify (int length, int offset, int k);
 int a [30], b [30], c [30];                                   //define a[],b[],c[] to store device's bus device function
 int i;
 
+
 /* main function */
 int main (void) {
-	PCI_scan ();
-	PCI_scan_keyboard ();
-	return 0;
+  PCI_scan ();
+  PCI_scan_keyboard ();
+  return 0;
 }
 
 /* PCI_scan Function */
 void PCI_scan (void) {
-	int bus, device, function;                               //define variates for PCI bus, device, function 
-	DWORD data;                                              //define data to receive data from PCI config data port
-	DWORD address;                                           //define address to write PCI address into PCI config index port
+  int bus, device, function;                               //define variates for PCI bus, device, function 
+  DWORD data;                                              //define data to receive data from PCI config data port
+  DWORD address;                                           //define address to write PCI address into PCI config index port
   system ("cls");
-	printf("Bus   Device   Function   VDID");
-	printf("\n");
+  printf("Bus   Device   Function   VDID");
+  printf("\n");
   i = 1;                                                   //must be initialized here
 
-	for (bus = 0; bus <= 0x63; ++bus) {
-		for (device = 0; device <= 0x1F; ++device) {
+  for (bus = 0; bus <= 0x63; ++bus) {
+	  for (device = 0; device <= 0x1F; ++device) {
 			for (function = 0; function <= 0x7; ++function) {
 				PCI_write (PCI_CONFIG_INDEX_PORT,PCI_ADDRESS);
 				data = PCI_read (PCI_CONFIG_DATA_PORT);
 				if ((WORD)data != 0xFFFF ) {
-					i++;
+          i++;
 					a [i] = bus;                                     //a[] receive bus device function values for global use
 					b [i] = device;                                  
 					c [i] = function;
@@ -101,35 +102,34 @@ DWORD PCI_read (int read) {
 	return data;
 }
 
-
 /* read PCI */
 /* read PCI in byte */
 void PCI_read_in_byte (int bus, int device, int function) {
-	DWORD data;
-	WORD data_0, data_1, data_2, data_3;                     //receive splited data from PCI_data
-	int offset = 0;
-	int i = 0;
+  DWORD data;
+  WORD data_0, data_1, data_2, data_3;                     //receive splited data from PCI_data
+  int offset = 0;
+  int i = 0;
   textcolor (GREEN);
-	system ("cls");
-	printf ("**************************************************\n");
-	printf ("*********Bus: %02X Device: %02X Function: %02X**********\n", bus,device,function);
-	printf ("**************************************************\n");
-	cprintf ("   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
+  system ("cls");
+  printf ("**************************************************\n");
+  printf ("*********Bus: %02X Device: %02X Function: %02X**********\n", bus,device,function);
+  printf ("**************************************************\n");
+  cprintf ("   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
   printf ("\n");
   cprintf ("%X0 ", i);
 
-	for (offset = 0; offset <= 0x3F; offset++) {
-		PCI_write (PCI_CONFIG_INDEX_PORT,PCI_ADDRESS_OFFSET);
-		data = PCI_read (PCI_CONFIG_DATA_PORT);
-		data_0 = (BYTE) (data);                                //split PCI_data 
-		data_1 = (BYTE) (data >> 8);
-		data_2 = (BYTE) (data >> 16);
-		data_3 = (BYTE) (data >> 24);
-		printf ("%02X %02X %02X %02X ", data_0, data_1, data_2, data_3);
+  for (offset = 0; offset <= 0x3F; offset++) {
+    PCI_write (PCI_CONFIG_INDEX_PORT,PCI_ADDRESS_OFFSET);
+    data = PCI_read (PCI_CONFIG_DATA_PORT);
+    data_0 = (BYTE) (data);                                //split PCI_data 
+    data_1 = (BYTE) (data >> 8);
+    data_2 = (BYTE) (data >> 16);
+    data_3 = (BYTE) (data >> 24);
+    printf ("%02X %02X %02X %02X ", data_0, data_1, data_2, data_3);
     if (offset < 63 && (offset+1) % 4 == 0) {
       i = i + 1;
       printf ("\n");
-			cprintf ("%X0 ", i);
+      cprintf ("%X0 ", i);
     }
 	}
 
@@ -168,91 +168,90 @@ void PCI_read_in_word (int bus, int device, int function) {
 
 /* read PCI in dword */
 void PCI_read_in_dword (int bus, int device, int function) {
-	DWORD data;
-	int offset = 0;
-	int i = 0;
+  DWORD data;
+  int offset = 0;
+  int i = 0;
   textcolor (GREEN);
-	system ("cls");
-	printf ("**************************************\n");
-	printf ("  Bus: %02X   Device: %02X   Function: %02X\n", bus,device,function);
-	printf ("**************************************\n");
-	cprintf ("   03020100 07060504 0B0A0908 0F0E0D0C");
+  system ("cls");
+  printf ("**************************************\n");
+  printf ("  Bus: %02X   Device: %02X   Function: %02X\n", bus,device,function);
+  printf ("**************************************\n");
+  cprintf ("   03020100 07060504 0B0A0908 0F0E0D0C");
   printf ("\n");
   cprintf ("%X0 ", i);
 
-	for (offset = 0; offset <= 0x3F; offset++) {
-		PCI_write (PCI_CONFIG_INDEX_PORT,PCI_ADDRESS_OFFSET);
-		data = PCI_read (PCI_CONFIG_DATA_PORT);
-		printf ("%08lX ", data);
-		if (offset < 63 && (offset+1) % 4 == 0) {
+  for (offset = 0; offset <= 0x3F; offset++) {
+    PCI_write (PCI_CONFIG_INDEX_PORT,PCI_ADDRESS_OFFSET);
+    data = PCI_read (PCI_CONFIG_DATA_PORT);
+    printf ("%08lX ", data);
+    if (offset < 63 && (offset+1) % 4 == 0) {
       i = i + 1;
-			printf ("\n");
-			cprintf ("%X0 ", i);
+      printf ("\n");
+      cprintf ("%X0 ", i);
     }
-	}
+  }
 
 	printf ("\n\nF1: BYTE  F2: WORD  F3: DWORD  ESC: RETURN ENTER: MODIFY");
 }
 
-
 /* keyboard */
 /* PCI_scan_keyboard Function */
 int PCI_scan_keyboard (void) {
-	int x = 1, y = 2;
+  int x = 1, y = 2;
 
-	for (;;) {
-		gotoxy (x, y);
-		switch (WAIT_PRESS) {
-			case ESC:
-				return 0;
-			case UP:
-				if (y > 2 && y <= i)
-					y = y - 1;
-				break;
-			case DOWN:
-				if (y > 1 && y < i)
-					y = y + 1;
-				break;
-			case ENTER:
-				PCI_read_in_byte (a [y], b [y], c [y]);
-				PCI_read_keyboard (y);
-				PCI_scan ();
-				x = 1;
-				y = 2;
-				break;
+  for (;;) {
+    gotoxy (x, y);
+    switch (WAIT_PRESS) {
+      case ESC:
+        return 0;
+      case UP:
+        if (y > 2 && y <= i)
+          y = y - 1;
+        break;
+      case DOWN:
+        if (y > 1 && y < i)
+          y = y + 1;
+        break;
+      case ENTER:
+        PCI_read_in_byte (a [y], b [y], c [y]);
+        PCI_read_keyboard (y);
+        PCI_scan ();
+        x = 1;
+        y = 2;
+        break;
 
-		}
-	}
+    }
+  }
 
 }
 
 /* PCI_read_keyboard */
 int PCI_read_keyboard (int k) {    //k receive
-	int x = 4, y = 5;
-	int x_start_limit = 4, y_start_limit = 5;
-	int x_end_limit = 49, y_end_limit = 20;
-	int x_change = 3, y_change = 1;
-	int length = 2;  //byte,word,dword character length
-	int offset = 0;
-	int x_offset_change = 1;
+  int x = 4, y = 5;
+  int x_start_limit = 4, y_start_limit = 5;
+  int x_end_limit = 49, y_end_limit = 20;
+  int x_change = 3, y_change = 1;
+  int length = 2;  //byte,word,dword character length
+  int offset = 0;
+  int x_offset_change = 1;
 
-	for (;;) {
-		gotoxy (1, 4);
+  for (;;) {
+    gotoxy (1, 4);
     printf ("%02X",offset);
-		gotoxy (x, y);
-		switch (WAIT_PRESS) {
-			case ESC:
-        return 0;
+    gotoxy (x, y);
 
-			case UP:
-				if (y > y_start_limit && y <= y_end_limit) {
-					y = y - y_change;
+    switch (WAIT_PRESS) {
+      case ESC:
+        return 0;
+      case UP:
+        if (y > y_start_limit && y <= y_end_limit) {
+          y = y - y_change;
           offset = offset - 16;
         }
-				break;
-			case DOWN:
-				if (y >= y_start_limit && y < y_end_limit) {
-					y = y + y_change;
+        break;
+      case DOWN:
+        if (y >= y_start_limit && y < y_end_limit) {
+          y = y + y_change;
           offset = offset + 16;
         }
         break; 
@@ -261,16 +260,15 @@ int PCI_read_keyboard (int k) {    //k receive
           x = x - x_change;
           offset = offset - x_offset_change;
         }
-				break;
-			case RIGHT:
-				if (x >= x_start_limit && x < x_end_limit) {
-					x = x + x_change;
+        break;
+      case RIGHT:
+        if (x >= x_start_limit && x < x_end_limit) {
+          x = x + x_change;
           offset = offset + x_offset_change;
         }
-				break;
-
-			case F1:
-				PCI_read_in_byte (a [k], b [k], c [k]);
+        break;
+      case F1:
+        PCI_read_in_byte (a [k], b [k], c [k]);
         x = 4;
         y = 5;
         x_end_limit = 49, y_end_limit = 20;
@@ -279,8 +277,8 @@ int PCI_read_keyboard (int k) {    //k receive
         offset = 0;
         x_offset_change = 1;
         break;
-			case F2:
-				PCI_read_in_word (a[k], b[k], c[k]); 
+      case F2:
+        PCI_read_in_word (a[k], b[k], c[k]); 
         x = 4;
         y = 5;
         x_end_limit = 39, y_end_limit = 20;
@@ -289,8 +287,8 @@ int PCI_read_keyboard (int k) {    //k receive
         offset = 0;
         x_offset_change = 2;
         break;
-			case F3:
-				PCI_read_in_dword (a [k], b[k], c[k]);
+      case F3:
+        PCI_read_in_dword (a [k], b[k], c[k]);
         x = 4;
         y = 5;
         x_end_limit = 31, y_end_limit = 20;
@@ -299,11 +297,11 @@ int PCI_read_keyboard (int k) {    //k receive
         offset = 0;
         x_offset_change = 4;
         break;
-
       case ENTER:       
         PCI_modify (length, offset, k);
         break;
     }
+
   }
 
 }
@@ -321,10 +319,10 @@ void PCI_modify (int length, int offset, int k) {
   int function = c [k];
   int select_offset;                                       
   int input_judgement = 0;                                 //judgement condition whether to excute if()
-	select_offset = offset % 4;
+  select_offset = offset % 4;
   offset = offset/4;
 
-	for (i = 1; (modify_input [i] = getchar ()) != 10 && i <= length; i++) {       
+  for (i = 1; (modify_input [i] = getchar ()) != 10 && i <= length; i++) {       
     if (modify_input [i] >= 48 && modify_input [i] <=57) {
       modify_input [i] = (DWORD) ((modify_input [i] - 48) * pow (16, (length - i)));
       input_change = input_change + modify_input [i];
@@ -381,6 +379,3 @@ void PCI_modify (int length, int offset, int k) {
   }
 
 }
-
-
-
